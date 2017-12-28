2dde0b94bf32dba7686d12157a51f33b33405c22
memesp <34920625+memesp@users.noreply.github.com>
2017-12-28 15:23:01 -0500

    dd

--- src/dsp.js
+++ src/dsp.js
@@ -5,9 +5,11 @@
    config file in this repo.
 */
 var CONFIG = require('./config.json');
+var auth = require('./auth.json');
 // Discord stuff
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const token = auth.token;
 var channelName = CONFIG.channel;   // Discord channel you want this to listen to for commands
 
 // database stuff
@@ -17,8 +19,6 @@ var dbUser = CONFIG.dbUser;
 var dbPass = CONFIG.dbPass;
 var dbName = CONFIG.dbName;
 var dbTable = CONFIG.dbTable;
-var countQuery = "SELECT count(*) FROM " + dbTable;
-var quoteQuery = "SELECT * FROM " + dbTable + " WHERE id==" + numQuotes;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -30,17 +30,18 @@ client.on('ready', () => {
       database : dbname
   }); connection.connect();
 });
-   
-});
 
 client.on('message', msg => {
   if (msg.channel.name == channelName && msg.content === '!dsp') {
-      var numQuotes = connection.query('SELECT count(*) FROM 
-      var quoteQuery = "SELECT * FROM " + dbTable + " WHERE id==" + numQuotes;
-      var quote;
-      connection.query(quoteQuery, function (error, results, fields) {
+      var countQuery = "SELECT count(*) FROM " + dbTable;
+      var numQuotes = connection.query(countQuery, function (error, result, fields) {
           if (error) throw error;
-          quote = results[0].solution;
+      });
+      var quoteQuery = "SELECT quotedata FROM " + dbTable + " WHERE id==" + numQuotes;
+      var quote = [];
+      connection.query(quoteQuery, function (error, result, fields) {
+          if (error) throw error;
+          quote = results[0].quotedata;
       });   
       msg.reply(quote);
   }
